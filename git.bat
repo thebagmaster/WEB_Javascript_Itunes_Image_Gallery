35767d144971ebae2b149e8cf1bae53199501d97
Daniel Nichols <thebagmaster@gmail.com>
2014-09-20 22:44:52 -0500

    First Commit

---
+++ git.bat
@@ -0,0 +1,7 @@
+git.exe init
+git.exe add .
+git.exe commit -m "First Commit"
+git.exe remote add origin https://github.com/thebagmaster/WEB_Javascript_Itunes_Image_Gallery.git
+git.exe pull https://github.com/thebagmaster/WEB_Javascript_Itunes_Image_Gallery.git
+git.exe push origin master
+pause
